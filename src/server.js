96d0f915e9db25d3c313b42ec136cd599bec804d
Indykatz <cats83@me.com>
2022-07-27 17:17:53 +0100

    /test

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 5001; // store supplied port or port 5001.
 app.use(express.json()); // use express json parser.
 app.use(cors()); // allows requests to be made from other Node applications of any origin.
 app.use(userRouter);
+app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 }); // listening on port 5001 or provided port on current location (localhost).
